8123bd2a4d1a553eb6f24c2dddbc1c97ec5c83ce
Sully <sully_2306@gorbulas.co.uk>
2018-11-09 12:18:09 +0000

    Updated paths file to reflect dynamic URL

--- config/paths.php
+++ config/paths.php
@@ -1,5 +1,5 @@
 <?php
 
-define('URL', 'http://zion');
+define('URL', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
 
 define('ROOT_DIR', realpath(__DIR__ . '/..'));
