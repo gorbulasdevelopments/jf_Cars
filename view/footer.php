309119675acfbcea335513d4e728bc8860513e13
Matthew Sullivan <sully_2306@gorbulas.co.uk>
2018-11-14 22:22:40 +0000

    Redesign for wide screen

--- view/footer.php
+++ view/footer.php
@@ -23,17 +23,18 @@
 				<a href="mailto:SALES@JFCARS.CO.UK" style="color: #ffffff;">SALES@JFCARS.CO.UK</a>
 			</div>
 			
-			<div style="width: 300px; height: 80px; margin: 0px auto; color: #ffffff; background-color: #ff3333; line-height: 80px;">
+			<div id="social_buttons">
 				Social Buttons
 			</div>
+			<div id="financial_disclosure">
 			<p><b>Financial Disclosure</b><br /><br />
 				JF Car Sales Ltd. act as a non-independent credit intermediary for a limited number of lenders and are able to offer a limited number of finance products from these providers. 
 				JF Cars Sales Ltd. is an Appointed Representative of AutoProtect (MBI) Limited who are authorised and regulated by the Financial Conduct Authority, their firm reference number is ######. 
 				You can check this on the FCA's website www.fca.gov.uk or by contacting the FCA on 0800 1116768. Our authorisation reference number is ######. 
 				For any concerns or queries relating to authorised products please contact us by emailing to sales@jfcars.co.uk.</p>
-
+			</div>
 		</div>
-		<div style="height: 60px; width: 100%; background-color: #ffffff;">
+		<div style="height: 60px; width: 100%; background-color: #ffffff; float: left;">
 			<div style="width: 300px; height: 100%; margin: 0px auto; color: #ff3333; line-height: 60px; text-align: center;">
 				COPYRIGHT JF CARS 2018 
 			</div>
