7642808afc56c3664538b3fe0a45facec214a031
sully <sully@zion>
2018-11-18 23:07:50 +0000

    Better error messages

--- view/error/index.php
+++ view/error/index.php
@@ -1,7 +1,17 @@
 <div id="content_container">
     <div id="content">
         <?php
-            echo $this->errorMessage;
+            if(isset($this->errorFunction)) {
+                echo "An error has been reported by <b>" . $this->errorFunction . "</b><br /><br />";
+
+                echo "The error message was: <br /><br />";
+            } else {
+                echo "This page has encountered an error<br /><br />";
+
+                echo "The error message was: <br /><br />";
+            }
+
+            echo "<span style=\"margin-left: 20px; color: red;\">" . $this->errorMessage . "</span>";
         ?>
     </div>
 </div>
