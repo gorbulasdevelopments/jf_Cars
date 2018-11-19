e5f8ca100fae668b070c097f3844c06e1e3c7881
sully <sully@zion>
2018-11-19 23:02:45 +0000

    Customer Review Widget

--- view/header.php
+++ view/header.php
@@ -40,6 +40,8 @@ $root_folder = $_SERVER['DOCUMENT_ROOT'] . "";
 	<link href="https://fonts.googleapis.com/css?family=Montserrat|Abel|Raleway" rel="stylesheet">
 	<script src="HTTPs://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo URL; ?>/view/public/functions.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
   </head>
 
   <body>
--- view/index/index.php
+++ view/index/index.php
@@ -11,7 +11,7 @@
 			?>
 	
 		<div id="content">
-		<div style="width: 100%; margin: 0px auto; text-align: center; margin-top: 80px; margin-bottom: 80px;">
+		<div style="width: 100%; margin: 0px auto; text-align: center; margin-top: 80px; margin-bottom: 40px;">
 			<h1>Welcome to <font style="color: red;">JF</font> CARS</h1>
 			<h3>THE BEST WAY TO BUY YOUR NEXT CAR</h3>
 			<br />
@@ -20,21 +20,80 @@
 			<p style="line-height: 30px; margin-bottom: 20px;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 		</div>
 		
-		<!--<div style="width: 100%; background-color: #444444; height: 500px; background-repeat: no-repeat; background-size: contain; position: relative;">
-		
-			<div style="width: 500px; margin: 0px auto; padding-top: 50px; height: 50px; text-align: center; ">
-				<h2 style="color: #ffffff;">Customer Reviews</h2>
-				<h3 style="margin-top: -20px;">WHAT OUR CUSTOMERS SAY ABOUT US</H3>
+		<div style="text-align: center;">
+			<h2>Latest additions to our stock</h2>
+
+			<div class="vehicle_container">
+				
+				<div class="vehicle_description">
+					<h2><a href="http://192.168.1.100/showroom/FORD/FOCUS/DS57NAO">2008 FORD FOCUS</a></h2>
+					<h3>Hello wolrd</h3>
 				</div>
-				<img src="public/images/review_2.png" style="width: 50%; position: absolute; left:50%; margin-left: -25%; top: 200px;" />
-			
+				<div class="vehicle_image">
+					<a href="http://192.168.1.100showroom/FORD/FOCUS/DS57NAO"><img src="http://192.168.1.100/media/media.jpg?id=Rk9SRC9GT0NVUy9EUzU3TkFPL0RTNTdOQU9fMTU0MjUzNjMxMy5qcGc="></a>				
+					<div class="vehicle_image_count">1 Image</div>
+				</div>
+				
+				<div class="vehicle_details">
+					<ul>
+						<li>ENGINE SIZE: 1596</li>
+						<li>MILEAGE: 5000</li>
+						<li>FUEL TYPE: PETROL</li>
+						<li>YEAR: 2008</li>
+						<li>TRANSMISSION: MANUAL</li>
+						<li>INSURANCE GROUP: 7</li>
+						<li>MPG: </li>
+						<li>ROAD TAX: G</li>
+					</ul>
+								</div>
+				
+				<div class="vehicle_price">
+					<h1>£20000</h1>
+				</div>
+			</div>
+		</div>
+		<div style="width: 100%; height: 500px; margin-top: 40px;">
+		
+			<div  id="myCarousel" style="margin: 0px auto; text-align: center;" class="carousel slide" data-ride="carousel" data-interval="10000">
+				<h2>Customer Reviews</h2>
+				<div class="spacer"></div>
+				<div class="carousel-inner">
+					<div class="item active">
+						<span class="reviewMessage">
+							Such a fantastic buying experience
+						</span>
+
+						<span class="reviewAuthor">
+							Matt S. 2018
+						</span>
+					</div>
+
+					<div class="item">
+						<span class="reviewMessage">
+							Such a fantastic buying experience 2
+						</span>
+
+						<span class="reviewAuthor">
+							Matt S. 2019
+						</span>
+					</div>
+
+					<div class="item">
+						<span class="reviewMessage">
+							Such a fantastic buying experience 3
+						</span>
+
+						<span class="reviewAuthor">
+							Matt S. 2020
+						</span>
+					</div>
+
+				</div>
+
+				
+			</div>
 		
 		</div>
-		
-		<div style="width: 960px; height: 500px; border: 1px solid #c0c0c0; margin: 0px auto; text-align: center; margin-top: 80px; position: relative;">
-			
-			<p style="position: absolute; left: 0px; top: 50%; width: 300px; height: 120px; padding: 10px; text-align: center; color: #fff; font-weight: bold; font-size: 24px; background-color: rgba(60, 72, 77, 0.6);"><br /><br />I found my perfect car!</p>
-		</div>-->
 		
 		<div style="clear: both"></div>
 </div>
