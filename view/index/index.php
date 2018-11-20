dca7a01f7a3b1484a510a235a872fe59f91fe638
Sully <sully_2306@gorbulas.co.uk>
2018-11-20 19:55:07 +0000

    Moved search bar, updated content

--- view/index/index.php
+++ view/index/index.php
@@ -1,36 +1,44 @@
 	<!-- Content Start -->
 	<div id="content_container">
 		<div id="index_banner">
-			<img src="<?php echo $images_directory ?>/slide-02.jpg" style="width: 100%;" />
-			
-			<p style="position: absolute; left: 0; top: 50%; width: 100%; text-align: center; color: #fff; font-weight: bold; font-size: 24px;">FIND YOUR PERFECT CAR TODAY</p>
-			
-		</div>
+		<div style="width: 850px; margin-top: 100px; z-index: 20; position: absolute; top: 300px; margin: 0px auto; left: 0; right: 0;">
 			<?php 
 				include ROOT_DIR . "/view/searchBar.php";
 			?>
+			</div>
+			<img src="<?php echo $images_directory ?>/slide-02.jpg" style="width: 100%;" />
+			
+			<p style="position: absolute; left: 0; top: 40%; width: 100%; text-align: center; color: #fff; font-weight: bold; font-size: 32px;">FIND YOUR PERFECT CAR TODAY</p>
+			
+		</div>
+
 	
 		<div id="content">
-		<div style="width: 100%; margin: 0px auto; text-align: center; margin-top: 80px; margin-bottom: 40px;">
+		<div style="width: 80%; margin: 0px auto; text-align: center; margin-top: 40px;">
 			<h1>Welcome to <font style="color: red;">JF</font> CARS</h1>
 			<h3>THE BEST WAY TO BUY YOUR NEXT CAR</h3>
 			<br />
-			<p style="line-height: 30px; margin-bottom: 20px;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-			<p style="line-height: 30px; margin-bottom: 20px;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-			<p style="line-height: 30px; margin-bottom: 20px;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-		</div>
-		
-		<div style="text-align: center;">
-			<h2>Latest additions to our stock</h2>
+			<p>If you are looking to purchase a quality used vehicle in the Kings Lynn area, then you have reached the right place! JF Car Sales are proud to offer you a excellent customer service on a wide range of vehicle, so why not check out our <a href="/showroom">showroom</a>?</p>
+			
+			<p>At JF Car Sales, we don't just sell you a vehicles, we sell you a professional car buying experience with the aim to make your purchase as easy as possible. </p>
 
+			<p>Our <a href="/showroom">showroom</a> is updated regularly in order to offer vehicles to suit everyone.</p>
+
+			<h3>Still can't find what you are looking for?</h3>
+			<p>Why not give us a call on <b>07500 123456</b> and we will try our best to find your perfect vehicle.</p>
+		</div>
+		<div class="spacer"></div>
+		<div style="text-align: center;">
+			<h1>Latest additions to our stock</h1>
+			<div class="spacer"></div>
 			<div class="vehicle_container">
 				
 				<div class="vehicle_description">
-					<h2><a href="http://192.168.1.100/showroom/FORD/FOCUS/DS57NAO">2008 FORD FOCUS</a></h2>
+					<h2><a href="/showroom/FORD/FOCUS/DS57NAO">2008 FORD FOCUS</a></h2>
 					<h3>Hello wolrd</h3>
 				</div>
 				<div class="vehicle_image">
-					<a href="http://192.168.1.100showroom/FORD/FOCUS/DS57NAO"><img src="http://192.168.1.100/media/media.jpg?id=Rk9SRC9GT0NVUy9EUzU3TkFPL0RTNTdOQU9fMTU0MjUzNjMxMy5qcGc="></a>				
+					<a href="/showroom/FORD/FOCUS/DS57NAO"><img src="/media/media.jpg?id=Rk9SRC9GT0NVUy9EUzU3TkFPL0RTNTdOQU9fMTU0MjUzNjMxMy5qcGc="></a>				
 					<div class="vehicle_image_count">1 Image</div>
 				</div>
 				
@@ -52,10 +60,11 @@
 				</div>
 			</div>
 		</div>
-		<div style="width: 100%; height: 500px; margin-top: 40px;">
+		<div class="spacer"></div>
+		<div style="width: 100%; height: 420px;">
 		
 			<div  id="myCarousel" style="margin: 0px auto; text-align: center;" class="carousel slide" data-ride="carousel" data-interval="10000">
-				<h2>Customer Reviews</h2>
+				<h1>Customer Reviews</h1>
 				<div class="spacer"></div>
 				<div class="carousel-inner">
 					<div class="item active">
@@ -70,21 +79,21 @@
 
 					<div class="item">
 						<span class="reviewMessage">
-							Such a fantastic buying experience 2
+							I found my perfect cars with JF Cars
 						</span>
 
 						<span class="reviewAuthor">
-							Matt S. 2019
+							Nikki S. 2017
 						</span>
 					</div>
 
 					<div class="item">
 						<span class="reviewMessage">
-							Such a fantastic buying experience 3
+							JF Cars offer a top notch service
 						</span>
 
 						<span class="reviewAuthor">
-							Matt S. 2020
+							Dave F. 14/02/2018
 						</span>
 					</div>
 
