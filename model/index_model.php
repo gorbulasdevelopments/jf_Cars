46b5256d06f232eec9c9e3743485af5779670087
Sully <sully_2306@gorbulas.co.uk>
2018-11-20 19:54:26 +0000

    Added function to get latest sale

--- model/index_model.php
+++ model/index_model.php
@@ -12,5 +12,11 @@ class Index_Model extends Model {
         return $strSQL->fetchAll();
     }
 
+    public function getLatestSale() {
+        $strSQL = $this->db->prepare("SELECT DISTINCT sale_price, sale_summary, vehicle_registration, vehicle_make, vehicle_model, vehicle_variant, vehicle_engine_size, vehicle_doors, vehicle_colour, vehicle_year, vehicle_mileage, vehicle_fuel, vehicle_transmission, vehicle_fuel_combined, vehicle_road_tax, vehicle_insurance_group, vehicle_extras, (SELECT vehicle_image_url FROM vehicle_image_table WHERE vehicle_cover_image = 1 and vehicle_id = vt.vehicle_id) as vehicle_image, (SELECT COUNT(vehicle_image_id) FROM vehicle_image_table WHERE vehicle_id = vt.vehicle_id ) as vehicle_image_count, st.sale_add_date FROM sale_table st join (vehicle_table vt left join vehicle_image_table vit on vt.vehicle_id = vit.vehicle_id) on st.vehicle_id = vt.vehicle_id  WHERE st.sale_complete_date IS NULL ORDER BY st.sale_add_date DESC LIMIT 1");
+		$strSQL->execute();
+        return $strSQL->fetchAll(PDO::FETCH_ASSOC);
+    }
+
 }
  
