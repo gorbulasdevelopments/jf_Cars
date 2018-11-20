3df96fb01b851b074b603edd5eea6e9a0cc62945
Sully <sully_2306@gorbulas.co.uk>
2018-11-20 19:55:22 +0000

    Added content

--- view/about/index.php
+++ view/about/index.php
@@ -7,7 +7,28 @@
 			?>
     <div id="content">
 <h2>About JF Cars</h2>
+<div class="spacer"></div>
 
- 
-    </div>
+
+
+<p>JF Car Sales offers a wide selection of nearly new and used cars, all include an intensive quality check to ensure you get the most from your purchase. <br /> <br />
+
+Established in 2018, our aim is to povide you with a professional experience from a familty run business making your next purchase as easy as possible. <br /><br />
+
+Located on the outskirts of King's Lynn, we have easy access from all majour roadwaysm with travel time of 1 hour from Norwich, Peterborough and Cambridge. </p>
+
+<p>JF Car Sales has only recently established, but our staff have years of experience in the motor industry. <br /><br />What makes us special is;</p>
+
+	<ul>
+	<li>All of our vehicles are fully checked by an independant mechanic</li>
+	<li>All vehicles with less than 6 months MOT come with a full 12 months MOT</li>
+    <li>All vehicles come with a 3 month warranty which can be extended</li>
+    <li>All of our customers important to us, you will be dealt with in a professional and friendly manner, in your own time and a "non pushy" environment</li>
+	</ul>
+
+<p>So what are you waiting for? Check out our <a href="/showroom">showroom</a> now! Or even like us on Facebook</p>
+	<div class="spacer"></div>
+	</div>
+
 </div>
+
